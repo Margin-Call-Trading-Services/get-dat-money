3cd10c6127fdbe61d546b1987fe2b33ff9f7cf55
ryanlattanzi <ryan.lattanzi@lions.nfl.net>
2023-03-15 16:21:43 -0400

    updating validators

--- api/validators.go
+++ api/validators.go
@@ -9,26 +9,14 @@ import (
 func validateDateOnlyFmt(timeStr string) error {
 	_, err := utils.ParseTimeStringDateOnly(timeStr)
 	if err != nil {
-		return err
+		return fmt.Errorf("invalid date: `%s`. ensure fmt YYYY-MM-DD", timeStr)
 	}
 	return nil
 }
 
 func validateInterval(interval string) error {
 	valid := []string{
-		"1m",
-		"2m",
-		"5m",
-		"15m",
-		"30m",
-		"60m",
-		"90m",
-		"1h",
 		"1d",
-		"5d",
-		"1wk",
-		"1mo",
-		"3mo",
 	}
 
 	c := utils.SliceContains(valid, interval)
